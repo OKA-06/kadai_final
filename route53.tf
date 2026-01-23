38c27f64cc4c097b37c4acfabac70d213c494188
oka <kurooka.1130@gmail.com>
2026-01-24 00:03:01 +0900

    route53 apply

--- route53.tf
+++ route53.tf
@@ -1,35 +1,32 @@
-#Route53
+# Route53 (draft)
 
-resource "aws_route53_zone" "primary" {
-  name = var.domain_name #ドメイン名は後で下に追加。terraform.tfvarsにも入れておく。
-  force_destroy = false
+data "aws_route53_zone" "primary" {
+  zone_id = "Z001362614ACG5RN7FNP3"
 }
 
-resource "Route53_record" "prod_a" {
-    zone_id = aws_route53_zone.primary.zone_id
-    name    = "prod-a.${var.domain_name}"
-    type    = "A"
+#本番用のレコード
+resource "aws_route53_record" "prod_a" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "cnnagoya.com"
+  type    = "A"
 
-    alias {
-      name                   = aws_lb.prod_a.dns_name
-      zone_id                = aws_lb.prod_a.zone_id
-      evaluate_target_health = true
-    }
-}
-resource "Route53_record" "dev_a" {
-    zone_id = aws_route53_zone.primary.zone_id
-    name    = "dev-a.${var.domain_name}"
-    type    = "A"
-
-    alias {
-      name                   = aws_lb.dev_a.dns_name
-      zone_id                = aws_lb.dev_a.zone_id
-      evaluate_target_health = false
-    }
-  
+  alias {
+    name                   = aws_cloudfront_distribution.main.domain_name
+    zone_id                = aws_cloudfront_distribution.main.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
 
-variable "domain_name" {
-  type    = string
-  default = "example.com" # 仮のやつ
+#検証用のレコード
+
+resource "aws_route53_record" "dev_a" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "dev.cnnagoya.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.main.domain_name
+    zone_id                = aws_cloudfront_distribution.main.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
